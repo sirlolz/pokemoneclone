c1a8809f1645cc048b6aa3b671a745c8512afca4
github username <github email address>
2019-09-10 14:30:25 -0700

    seed data updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,30 +14,6 @@ Pack.destroy_all
 raw_response = RestClient.get('https://pokeapi.co/api/v2/pokemon?limit=151')
 data = JSON.parse(raw_response)
 pokemon_info = {}
-<<<<<<< HEAD
-    data['results'].each do |p|
-        pokemon_info[:name] = p['name']
-        pokemon_stats = JSON.parse(RestClient.get(p['url']))
-        pokemon_info[:hp] = pokemon_stats['stats'][5]['base_stat']
-        pokemon_info[:att] = pokemon_stats['stats'][4]['base_stat']/2
-        pokemon = Pokemon.new(pokemon_info)
-        pokemon.save
-    end
-j = User.new(name:'jared')
-j.save
-t = Team.new(user: j, pokemon: Pokemon.all.first)
-t.save
-c=1
-5.times do 
-    Npc.create(name: "npc#{c}")
-    c+=1
-end
-count = 1
-5.times do 
-    Pack.create(npc: Npc.all.first(count).last, pokemon: Pokemon.all.first(count).last)
-    count += 1
-end
-=======
 
 data['results'].each do |p|
     pokemon_info[:name] = p['name']
@@ -52,4 +28,13 @@ end
 user1 = User.create(name:'user1')
 team1 = Team.create(user: user1, pokemon: Pokemon.all.first)
 
->>>>>>> joseph
+5.times do 
+    Npc.create(name: "npc#{c}")
+    c+=1
+end
+count = 1
+5.times do 
+    Pack.create(npc: Npc.all.first(count).last, pokemon: Pokemon.all.first(count).last)
+    count += 1
+end
+
