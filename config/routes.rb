e9048b37194be5d7d45acdb88ed91a1a5245f75e
zozep <jpark041@gmail.com>
2019-09-09 18:30:44 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :teams
   resources :npcs
   resources :pokemons
-  resources :users
+  resources :users :only [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
