8cf4c72d671bcc98f6d00d3ddb769ac3df4769ea
github username <github email address>
2019-09-09 18:39:34 -0700

    create personal working branch

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -1,2 +1,17 @@
 class NpcsController < ApplicationController
+    def index
+    end
+    def show
+    end
+    def new
+    end
+    def create
+    end
+    def destroy
+    end
+    def edit
+    end
+    def update
+    end
+
 end
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,4 @@
 class PokemonsController < ApplicationController
+    def index 
+    end
 end
