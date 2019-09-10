8690f92c43ae68ca929638719147eb7959772808
github username <github email address>
2019-09-09 20:43:26 -0700

    created restful routes for npc

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -7,19 +7,31 @@ class NpcsController < ApplicationController
     end
 
     def new
+        @npc = Npc.new
     end
 
     def create
+        @npc = Npc.create(name: params[:npc][:name])
+        Pack.create(npc: @npc, pokemon: Pokemon.find(params[:npc][:pokemon_ids]))
+        redirect_to npc_path(@npc)
     end
 
     def destroy
+        npc = Npc.find(params[:id])
+        npc.destroy
+        redirect_to npcs_path
     end
 
     def edit
+        @npc = Npc.find(params[:id])
     end
 
     def update
-
+        @npc = Npc.find(params[:id])
+        @npc.update(name: params[:npc][:name])
+        @npc.packs.destroy_all
+        Pack.create(npc: @npc, pokemon: Pokemon.find(params[:npc][:pokemon_ids]))
+        redirect_to npc_path(@npc)
     end
 
 end
