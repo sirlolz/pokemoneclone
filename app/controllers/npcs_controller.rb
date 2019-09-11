7ae6b7889315a693be699a5fd97b5717647a98d8
github username <github email address>
2019-09-10 17:01:41 -0700

    added pack hp creation to npc update

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -35,7 +35,7 @@ class NpcsController < ApplicationController
         @npc = Npc.find(params[:id])
         @npc.update(name: params[:npc][:name])
         @npc.packs.destroy_all
-        Pack.create(npc: @npc, pokemon: Pokemon.find(params[:npc][:pokemon_ids]))
+        pack_instance = Pack.create(npc: @npc, pokemon: Pokemon.find(params[:npc][:pokemon_ids]))
         pack_instance.update(hp: pack_instance.pokemon.hp)
         redirect_to npc_path(@npc)
     end
